73e66966639b52a15f1326cb55da56204ed980dc
Serge <Sergey-kolyhalov@rambler.ru>
2021-09-03 20:33:31 +0300

    http://127.0.0.1:8000/todo  - при обновлении страницы постояно будет выводить строчки на разных позициях

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -2,13 +2,22 @@ from django.http import HttpResponse
 
 from django.views import View
 
+import random
+
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+        temp = ['<li>Установить python</li>',
+                '<li>Установить django</li>',
+                '<li>Запустить сервер</li>',
+                '<li>Порадоваться результату</li>',
+                '<li>Конец</li>']
+        send_text = []
+        for i in range(len(temp)):
+            temp_1 = random.choice(temp)
+            send_text.append(temp_1)
+            temp.pop(temp.index(temp_1))
+        send_text_last = '<ul>' + send_text[0] + send_text[1] + send_text[2] + send_text[3] + send_text[4] + '</ul>'
+
+        return HttpResponse(send_text_last)
