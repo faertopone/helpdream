102860be32982a5c0aaf8fa5c9a5ed4bc2828afd
Serge <Sergey-kolyhalov@rambler.ru>
2022-01-25 16:20:37 +0300

    module 10 Осталось только с еррорами)

--- web_blog/app_blog/static/js/index.js
+++ web_blog/app_blog/static/js/index.js
@@ -1,32 +1,10 @@
 window.addEventListener('DOMContentLoaded', function(){
 
+
   let selector = document.querySelectorAll('input[type="tel"]');
+
   let im = new Inputmask('+7 (999) 999-99-99')
-
   im.mask(selector);
-
-  // Или так если пропистаь дата атрибут непосредственно в html
-  // let im = new Inputmask()
-
-
-
-
-  //
-  // let validateForms = function (selector, rules, succesModal, YaGoal) {
-  //   new window.JustValidate(selector, {
-  //     rules: rules,
-  //
-  //   });
-  // }
-  //
-  // validateForms('.register_form',
-  //     {
-  //       email:
-  //           {required: true, email: true},
-  //       tel: {required: true}
-  //     }, '.thanks-popup', 'send goal'
-  //     )
-
 
 
   const swiper = new Swiper('.swiper', {
