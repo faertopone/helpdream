9842cae7216fe7bb552e55497e37bb4da0ead52e
Serge <Sergey-kolyhalov@rambler.ru>
2022-01-28 19:58:54 +0300

    module 12 вот так?)

--- web_blog/app_blog/admin.py
+++ web_blog/app_blog/admin.py
@@ -4,10 +4,20 @@ from django.utils.translation import gettext_lazy as _
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'min_description', 'creadet_at', 'id']
+    list_display = ['title', 'min_description_admin', 'creadet_at', 'id']
     list_filter = ['title', 'creadet_at']
     search_fields = ['title', 'creadet_at']
     fields = ['author', 'title', 'description']
+
+    #Это сделал для того что бы в админ панели выводилось краткое описание
+    def min_description_admin(self, obj):
+        text_str = obj.description
+        text = list(text_str)
+        if len(text) > 15:
+            obj.description = text_str[:15] + '...'
+        return obj.description
+
+    min_description_admin.short_description = _('Descriptions mini')
 
 
 admin.site.register(Blog, BlogAdmin)
