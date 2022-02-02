6cfc71001504355db9b5b7a55eee86d1dbf5ba51
Serge <Sergey-kolyhalov@rambler.ru>
2022-02-03 02:54:00 +0300

    module 15 вот так?)

--- web_shops/rest/urls_json.py
+++ web_shops/rest/urls_json.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ItemList
+from .views import ItemList, ItemDetail
 
 urlpatterns = [
-    path('items/', ItemList.as_view(), name='json_items')
+    path('items/', ItemList.as_view(), name='json_items'),
+    path('items/<int:pk>/', ItemDetail.as_view(), name='items_detail')
 ]
