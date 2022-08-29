859d36a0e1a9a973e23381d9add10828cb965f2b
Sergey <112338398+faertopone@users.noreply.github.com>
2022-08-29 18:42:05 +0300

    Delete 04_DatabasesAndModels directory

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-from .views import AdvertisementListView, AdvertisementDetailView
-
-urlpatterns = [
-    path('', views.advertisement_index, name='advertisement_index'),
-    path('advertisements/', AdvertisementListView.as_view(), name='advertisements-list'),
-    path('advertisements/<int:pk>', AdvertisementDetailView.as_view(), name='advertisements-detail'),
-]
