30b35dda15020c3fa879b52a575a803ad7508de8
Sergey <112338398+faertopone@users.noreply.github.com>
2022-08-29 18:46:02 +0300

    Delete web_shops(REST_API) directory

--- web_shops(REST_API)/rest/urls_json.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ItemList, ItemDetail
-
-urlpatterns = [
-    path('items/', ItemList.as_view(), name='json_items'),
-    path('items/<int:pk>/', ItemDetail.as_view(), name='items_detail')
-]
