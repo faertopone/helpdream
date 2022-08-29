2c1a7cb7b071695aa565ec61ecb607edfe90ce7d
Sergey <112338398+faertopone@users.noreply.github.com>
2022-08-29 18:42:35 +0300

    Delete 07_DjangoAuthentication directory

--- 07_DjangoAuthentication/customauth/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class AuthForm(forms.Form):
-    mail = forms.EmailField(label='Введите почтовый адрес')
-    password = forms.CharField(label='Введите пароль', widget=forms.PasswordInput)
